54693ab3e353b8304bfb6fdb7db143045e98bef3
Lidya1234 <lidya94gher@gmail.com>
2021-02-24 16:27:49 +0300

    Add messager.js file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { showMessage } from './messager';
+
+showMessage('Somebody else did this work!');
+
+
+const messageEl = document.createElement('div');
+messageEl.textContent = 'I was put here by JavaScript!';
+document.body.appendChild(messageEl);
