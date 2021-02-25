4a50077a10253951821c6486967fe95933c57a3a
Lidya1234 <lidya94gher@gmail.com>
2021-02-25 16:10:41 +0300

    Add define plig in in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -47,19 +47,19 @@ module.exports = {
     new webpack.DefinePlugin({
         'typeof CANVAS_RENDERER': JSON.stringify(true),
         'typeof WEBGL_RENDERER': JSON.stringify(true)
-      })
+      }),
+    
+        // new CopyWebpackPlugin({
+        //   patterns: [
+        //     { from: path.resolve(__dirname, 'index.html'), to: path.resolve(__dirname, 'dist') },
+        //     { from: path.resolve(__dirname, 'assets'), to: path.resolve(__dirname, 'dist/assets') },
+        //   ],
+        // }),
   ],
 
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
   },
 
-//   plugins: [
-//     new CopyWebpackPlugin([
-//       {
-//         from: path.resolve(__dirname, 'index.html'),
-//         to: path.resolve(__dirname, 'dist')
-//       }
-//     ])
-//   ]
+
 };
